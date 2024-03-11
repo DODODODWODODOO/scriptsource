6e59926c62172139d22ac5a7b178e1ae17a308a8
DongWan <dlehddhks125@naver.com>
2024-03-11 17:58:14 +0900

    2024-03-11

---
+++ event/tab.js
@@ -0,0 +1,55 @@
+// orange
+// li 클릭 시 orange 클래스명 움직이기
+
+// show
+// 첫번째 li 클릭시 첫번째 div 보여주기
+
+// 3 개의 li 찾기
+const firstLi = document.querySelector(".list li:first-child");
+const secondLi = document.querySelector(".list li:nth-child(2)");
+const thirdLi = document.querySelector(".list li:last-child");
+
+// 3 개의 div 찾기
+const firstDiv = document.querySelector(".container div:nth-child(2)");
+const secondDiv = document.querySelector(".container div:nth-child(3)");
+const thirdDiv = document.querySelector(".container div:nth-child(4)");
+
+firstLi.addEventListener("click", () => {
+  // 다른 li 에 orange 클래스명 제거
+  secondLi.classList.remove("orange");
+  thirdLi.classList.remove("orange");
+  // firstLi 에 orange 클래스명 추가
+  firstLi.classList.add("orange");
+
+  // 다른 div 의 show 제거
+  secondDiv.classList.remove("show");
+  thirdDiv.classList.remove("show");
+  // 1번째 div 의 show 추가
+  firstDiv.classList.add("show");
+});
+secondLi.addEventListener("click", () => {
+  // 다른 li 에 orange 클래스명 제거
+  firstLi.classList.remove("orange");
+  thirdLi.classList.remove("orange");
+  // secondLi 에 orange 클래스명 추가
+  secondLi.classList.add("orange");
+
+  // 다른 div 의 show 제거
+  firstDiv.classList.remove("show");
+  thirdDiv.classList.remove("show");
+  // 2번째 div 의 show 추가
+  secondDiv.classList.add("show");
+});
+thirdLi.addEventListener("click", () => {
+  // 다른 li 에 orange 클래스명 제거
+  firstLi.classList.remove("orange");
+  secondLi.classList.remove("orange");
+  // thirdLi 에 orange 클래스명 추가
+  thirdLi.classList.add("orange");
+
+  // 다른 div 의 show 제거
+  firstDiv.classList.remove("show");
+  secondDiv.classList.remove("show");
+  // 3번째 div 의 show 추가
+  thirdDiv.classList.add("show");
+});
